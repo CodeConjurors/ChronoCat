3af1bd86542ec6fdd89f7822b9cb551a74cce4b9
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-01-04 14:49:57 +0200

    Create backend API route for deleting an activity

--- backend/src/main/java/com/chronocat/backend/BackendController.java
+++ backend/src/main/java/com/chronocat/backend/BackendController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /** The backend main application and its API routes. */
@@ -26,6 +30,12 @@ public class BackendController {
     @PostMapping("/api/activities")
     public Activity create(@RequestBody Activity activity) {
         return activityRepository.save(activity);
+    }
+
+    @DeleteMapping("/api/activities/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        activityRepository.deleteById(id);
     }
 
     public static void main(String[] args) {
