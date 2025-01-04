d8e5b4239a9fa7909c60c33d811ad64b4cabb22c
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-01-04 14:32:28 +0200

    Switch from @Controller to @RestController

--- backend/src/main/java/com/chronocat/backend/BackendController.java
+++ backend/src/main/java/com/chronocat/backend/BackendController.java
@@ -5,28 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /** The backend main application and its API routes. */
 @SpringBootApplication
-@Controller
+@RestController
 public class BackendController {
 
     @Autowired
     private ActivityRepository activityRepository;
 
     @GetMapping("/api/activities")
-    @ResponseBody
     public List<Activity> list() {
         return activityRepository.findAll();
     }
 
     @PostMapping("/api/activities")
-    @ResponseBody
     public Activity create(@RequestBody Activity activity) {
         return activityRepository.save(activity);
     }
