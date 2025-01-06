84b16be843ca6baeae728a00992bf6a3457aae37
Amanda <amandahamynen@users.noreply.github.com>
2025-01-06 10:51:20 +0200

    Create MVP of the frontend with hard-coded data

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { useState } from 'react'
 
-export default function App() {
+let DATA = [
+  {
+    time: '13:00',
+    name: 'Lunch',
+  },
+  {
+    time: '14:00',
+    name: 'Walk',
+  },
+  {
+    time: '18:00',
+    name: 'Dinner',
+  },
+]
+
+type ActivityProps = { time: string; name: string; deleteActivity: () => void }
+
+const Activity = ({ time, name, deleteActivity }: ActivityProps) => (
+  <View style={styles.item}>
+    <Text style={styles.item}>
+      {time} - {name}
+    </Text>
+    <Button title='Delete' onPress={deleteActivity} />
+  </View>
+)
+
+const App = () => {
+  const [activities, setActivities] = useState(DATA)
+  const [time, setTime] = useState('')
+  const [name, setName] = useState('')
+
+  const addActivity = () => {
+    setActivities([...activities, { time, name }])
+    setTime('')
+    setName('')
+  }
+
+  const deleteActivity = (item: { time: string; name: string }) => {
+    setActivities(
+      activities.filter(activity => !(activity.time === item.time && activity.name === item.name))
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View>
+          <View style={styles.item}>
+            <Text>Activities</Text>
+            <FlatList
+              data={activities}
+              renderItem={({ item }) => (
+                <Activity
+                  time={item.time}
+                  name={item.name}
+                  deleteActivity={() => deleteActivity(item)}
+                />
+              )}
+            />
+          </View>
+          <View style={styles.item}>
+            <Text>Add a new activity</Text>
+            <TextInput placeholder='Time' onChangeText={newTime => setTime(newTime)} value={time} />
+            <TextInput placeholder='Name' onChangeText={newName => setName(newName)} value={name} />
+            <Button title='Add' onPress={addActivity} />
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  item: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginVertical: 10,
+    marginHorizontal: 16,
+    fontSize: 20,
   },
-});
+})
+
+export default App
