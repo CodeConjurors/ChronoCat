f1a7b2ca654bddda3b009cef6152b659102d932d
Amanda <amandahamynen@users.noreply.github.com>
2025-01-21 14:55:13 +0200

    Update GET /api/activities to return a list sorted by index

--- backend/src/main/java/com/chronocat/backend/BackendController.java
+++ backend/src/main/java/com/chronocat/backend/BackendController.java
@@ -3,6 +3,7 @@ package com.chronocat.backend;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +24,7 @@ public class BackendController {
 
     @GetMapping
     public List<Activity> list() {
-        return activityRepository.findAll();
+        return activityRepository.findAll(Sort.by(Sort.Direction.ASC, "index"));
     }
 
     @PostMapping
