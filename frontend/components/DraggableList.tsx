ab8e371a76b35f60a106422fd3cc474780805b4a
Ville Sirviö <VSirvio@users.noreply.github.com>
2025-02-15 14:48:13 +0200

    Update DraggableList to its latest version

--- frontend/components/DraggableList.tsx
+++ frontend/components/DraggableList.tsx
@@ -1,124 +1,331 @@
-import { useEffect, useRef, useState } from 'react'
+import {
+  ElementRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 
-import { FlatList, View } from 'react-native'
+import {
+  FlatList,
+  GestureResponderEvent,
+  ListRenderItem,
+  MeasureOnSuccessCallback,
+  Text,
+  View,
+} from 'react-native'
+
+type MeasureCallback = MeasureOnSuccessCallback
+
+type DraggableListItemState = 'normal' | 'highlighted' | 'moving'
+
+interface DraggableListRenderItemInfo<ItemT> extends ListRenderItemInfo<ItemT> {
+  state: DraggableListItemState
+}
+
+type DraggableListRenderItem<ItemT> = (
+  info: DraggableListRenderItemInfo<ItemT>,
+) => React.ReactElement | null
+
+type DraggableListProps<ItemT> = {
+  data: ItemT[]
+  setData: (newData: ItemT) => void
+  renderItem: DraggableListRenderItem<ItemT>
+  keyExtractor: (item: ItemT, index: number) => string
+  onDragEnd: (from: number, to: number) => void
+  draggingEnabled: boolean
+  ListEmptyComponent: React.ReactElement
+  style: React.CSSProperties
+}
 
 /**
- * List whose items can be reordered by dragging. At the moment dragging mode
- * cannot be turned off for the list and the animations are not very smooth.
- * (The list item is just highlighted, when you start dragging it. It does not
- * appear to move, until you drag it onto another item.)
+ * List whose items can be reordered by dragging.
  *
  * @component
  * @example
+ * const ListEmptyMessage = () => (
+ *   <Text style={{ marginHorizontal: 10 }}>No items</Text>
+ * )
+ *
  * const App = () => {
  *   const [data, setData] = useState(['First', 'Second', 'Third'])
+ *   const [draggingEnabled, setDraggingEnabled] = useState(false)
  *
- *   const renderItem = ({ item, index, isActive }) => {
- *     const bgColor = isActive ? '#fcc8c2' : '#f9c2ff'
- *     const itemStyle = { backgroundColor: bgColor, padding: 16, margin: 10 }
+ *   const renderItem = ({ item, state }) => {
+ *     const itemStyle = {
+ *       backgroundColor: state === 'normal' ? '#f9c2ff' : '#fcc8c2',
+ *       flexGrow: 1,
+ *       padding: 16,
+ *       margin: 10,
+ *       color: state === 'highlighted' ? 'transparent' : null,
+ *       opacity: state === 'highlighted' ? 0.5 : 1,
+ *     }
  *     return <Text style={itemStyle}>{item}</Text>
  *   }
  *
  *   return (
- *     <DraggableList
- *       style={{ marginTop: 50 }}
- *       data={data}
- *       setData={setData}
- *       renderItem={renderItem}
- *       onDragEnd={(from, to) => console.log(`${from}->${to}`)}
- *     />
+ *     <View>
+ *       <DraggableList
+ *         style={{ marginTop: 50 }}
+ *         data={data}
+ *         setData={setData}
+ *         renderItem={renderItem}
+ *         onDragEnd={(from, to) => console.log(`${from}->${to}`)}
+ *         draggingEnabled={draggingEnabled}
+ *         ListEmptyComponent={<ListEmptyMessage />}
+ *       />
+ *       <View style={{ margin: 15 }}>
+ *         <Button
+ *           title={draggingEnabled ? 'Stop Editing' : 'Edit'}
+ *           onPress={() => setDraggingEnabled(!draggingEnabled)}
+ *           color={draggingEnabled ? 'lightgray' : 'steelblue'}
+ *         />
+ *       </View>
+ *     </View>
  *   )
  * }
  */
-const DraggableList = ({ data, setData, renderItem, keyExtractor, onDragEnd, style }) => {
-  const [itemYBounds, setItemYBounds] = useState(Array(data.length).fill(0))
-  const [startIndex, setStartIndex] = useState(null)
-  const [selected, setSelected] = useState(null)
+const DraggableList = <ItemT,>(props: DraggableListProps<ItemT>) => {
+  const itemYBounds = useRef(Array(props.data.length + 1).fill(0)).current
+  const [handleBound, setHandleBound] = useState(0)
+  const [startIndex, setStartIndex] = useState<number | null>(null)
+  const [selected, setSelected] = useState<number | null>(null)
+  const [pan, setPan] = useState(0)
+  const [offset, setOffset] = useState(0)
+  const [scrollOffset, setScrollOffset] = useState(0)
 
-  const onStart = evt => {
-    for (let i = 0; i < itemYBounds.length; i++) {
-      if (evt.nativeEvent.pageY < itemYBounds[i]) {
-        setStartIndex(i)
-        setSelected(i)
+  // TODO: Find out if this useEffect is actually needed
+  useEffect(() => {
+    if (itemYBounds.length - 1 > props.data.length) {
+      const removeCount = itemYBounds.length - props.data.length - 1
+      itemYBounds.splice(-removeCount, removeCount)
+    } else if (itemYBounds.length - 1 < props.data.length) {
+      const appendCount = props.data.length - itemYBounds.length + 1
+      itemYBounds.push(...Array<number>(appendCount).fill(0))
+    }
+  }, [props.data])
+
+  const shouldRespondToTouch = (evt: GestureResponderEvent) => {
+    return props.draggingEnabled && evt.nativeEvent.pageX > handleBound
+  }
+
+  const onStart = (evt: GestureResponderEvent) => {
+    for (let i = 1; i < itemYBounds.length; i++) {
+      if (evt.nativeEvent.pageY < itemYBounds[i] - scrollOffset) {
+        setOffset(-(evt.nativeEvent.pageY - (itemYBounds[i - 1] - scrollOffset)))
+        setPan(evt.nativeEvent.pageY)
+        setStartIndex(i - 1)
+        setSelected(i - 1)
         break
       }
     }
   }
 
-  const onMove = evt => {
+  const onMove = (evt: GestureResponderEvent) => {
     if (selected === null) {
       return
     }
 
-    for (let i = 0; i < itemYBounds.length; i++) {
-      if (evt.nativeEvent.pageY < itemYBounds[i]) {
-        if (i !== selected) {
-          const dataWoSelected = [
-            ...data.slice(0, selected),
-            ...data.slice(selected + 1),
+    setPan(evt.nativeEvent.pageY)
+
+    const selectedItemHeight = itemYBounds[selected + 1] - itemYBounds[selected]
+    const selectedItemMiddle = pan + offset + selectedItemHeight / 2
+    for (let i = 1; i < itemYBounds.length; i++) {
+      if (selectedItemMiddle < itemYBounds[i] - scrollOffset) {
+        if (i - 1 !== selected) {
+          const dataWithoutSelected = [
+            ...props.data.slice(0, selected),
+            ...props.data.slice(selected + 1),
           ]
-          setData([
-            ...dataWoSelected.slice(0, i),
-            data[selected],
-            ...dataWoSelected.slice(i),
+          props.setData([
+            ...dataWithoutSelected.slice(0, i - 1),
+            props.data[selected],
+            ...dataWithoutSelected.slice(i - 1),
           ])
-          setSelected(i)
+          setSelected(i - 1)
         }
         break
       }
     }
   }
 
-  const onStop = evt => {
+  const onStop = () => {
     setSelected(null)
-    onDragEnd(startIndex, selected)
+    props.onDragEnd(startIndex, selected)
   }
 
-  const draggableRenderItem = ({ item, index }) => (
-    <DraggableItem
-      content={renderItem({ item, index, isActive: selected === index })}
-      index={index}
-      itemYBounds={itemYBounds}
-      setItemYBounds={setItemYBounds}
-    />
+  const draggableRenderItem: ListRenderItem<ItemT> = ({ item, index }) => {
+    const content = props.renderItem({
+      item: item,
+      index: index,
+      state: selected === index ? 'highlighted' : 'normal',
+    })
+
+    return (
+      <DraggableItem
+        content={content}
+        index={index}
+        itemCount={props.data.length}
+        itemYBounds={itemYBounds}
+        setHandleBound={setHandleBound}
+        isSelected={selected === index}
+        draggingEnabled={props.draggingEnabled}
+      />
+    )
+  }
+
+  const movingItem = selected !== null && (
+    <View
+      style={{
+        position: 'absolute',
+        top: pan + offset,
+        zIndex: 10,
+        opacity: 0.8,
+        width: '100%',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+      }}
+    >
+      {props.renderItem(
+        { item: props.data[selected], index: selected, state: 'moving' },
+      )}
+      <DraggingHandle />
+    </View>
   )
 
   return (
     <View
-      onStartShouldSetResponder={evt => true}
+      onStartShouldSetResponder={shouldRespondToTouch}
       onResponderGrant={onStart}
       onResponderMove={onMove}
       onResponderRelease={onStop}
     >
+      {movingItem}
       <FlatList
-        data={data}
+        data={props.data}
         renderItem={draggableRenderItem}
-        keyExtractor={keyExtractor}
-        style={style}
+        keyExtractor={props.keyExtractor}
+        style={props.style}
+        onScroll={evt => setScrollOffset(evt.nativeEvent.contentOffset.y)}
+        scrollEnabled={!props.draggingEnabled}
+        ListEmptyComponent={props.ListEmptyComponent}
       />
     </View>
   )
 }
 
-const DraggableItem = ({ content, index, itemYBounds, setItemYBounds }) => {
-  const itemRef = useRef(null)
-  const [bottomYPos, setBottomYPos] = useState(0)
+type DraggingHandleProps = {
+  passOnLayout: (event: LayoutChangeEvent) => void
+  isSelected: boolean
+}
 
-  const doMeasure = () => {
-    itemRef.current?.measure((x, y, width, height, pageX, pageY) => {
+const DraggingHandle = forwardRef((props: DraggingHandleProps, ref) => (
+  <Text
+    style={{
+      fontSize: 35,
+      paddingHorizontal: 15,
+      paddingBottom: 5,
+      opacity: props.isSelected ? 0 : 1,
+    }}
+    onLayout={props.passOnLayout}
+    ref={ref}
+  >
+    ≡
+  </Text>
+))
+
+type DraggableItemProps = {
+  content: React.ReactElement
+  index: number
+  itemCount: number
+  itemYBounds: number[]
+  setHandleBound: (newBound: number) => void
+  isSelected: boolean
+  draggingEnabled: boolean
+}
+
+const DraggableItem = (props: DraggableItemProps) => {
+  const itemRef = useRef<ElementRef<typeof View>>(null)
+  const handleRef = useRef<ElementRef<typeof View>>(null)
+  const [bottomYPos, setBottomYPos] = useState(0)
+  const [topYPos, setTopYPos] = useState(0)
+  const [handleXPos, setHandleXPos] = useState(0)
+
+  const measureItem = () => {
+    const callback: MeasureCallback = (x, y, width, height, pageX, pageY) => {
       setBottomYPos(pageY + height)
-    })
+      setTopYPos(pageY)
+    }
+    itemRef.current?.measure(callback)
   }
 
   useEffect(() => {
-    setItemYBounds([
-      ...itemYBounds.slice(0, index),
-      bottomYPos,
-      ...itemYBounds.slice(index + 1)
-    ])
+    if (props.itemYBounds.length - 1 > props.itemCount) {
+      const removeCount = props.itemYBounds.length - props.itemCount - 1
+      props.itemYBounds.splice(-removeCount, removeCount)
+    } else if (props.itemYBounds.length - 1 < props.itemCount) {
+      const appendCount = props.itemCount - props.itemYBounds.length + 1
+      props.itemYBounds.push(...Array<number>(appendCount).fill(0))
+    }
+
+    props.itemYBounds[props.index + 1] = bottomYPos
+
+    if (props.itemYBounds.length > 1 &&
+        typeof props.itemYBounds[0] === 'number' &&
+        props.itemYBounds[0] !== NaN &&
+        typeof props.itemYBounds[1] === 'number' &&
+        props.itemYBounds[1] !== NaN) {
+      const itemHeight = props.itemYBounds[1] - props.itemYBounds[0]
+      let newItemYBounds = [...props.itemYBounds]
+      for (let i = 2; i < newItemYBounds.length; i++) {
+        newItemYBounds[i] = newItemYBounds[i - 1] + itemHeight
+      }
+      props.itemYBounds.splice(0, props.itemYBounds.length)
+      props.itemYBounds.push(...newItemYBounds)
+    }
   }, [bottomYPos])
 
-  return <View onLayout={doMeasure} ref={itemRef}>{content}</View>
+  if (props.index === 0) {
+    useEffect(() => {
+      props.itemYBounds[0] = topYPos
+    }, [topYPos])
+  }
+
+  const measureHandle = () => {
+    const callback: MeasureCallback = (x, y, width, height, pageX) => {
+      setHandleXPos(pageX)
+    }
+    handleRef.current?.measure(callback)
+  }
+
+  useEffect(() => {
+    if (typeof handleXPos === 'number' && handleXPos !== NaN) {
+      props.setHandleBound(handleXPos)
+    }
+  }, [handleXPos])
+
+  return (
+    <View
+      onLayout={measureItem}
+      ref={itemRef}
+      style={{
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+      }}
+    >
+      {props.content}
+      {
+        props.draggingEnabled &&
+        <DraggingHandle
+          passOnLayout={measureHandle}
+          ref={handleRef}
+          isSelected={props.isSelected}
+        />
+      }
+    </View>
+  )
 }
 
 export default DraggableList
